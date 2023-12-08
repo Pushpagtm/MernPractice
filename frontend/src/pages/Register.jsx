e0fbe5472990b3a876495192b108c8f5b18927fa
Pushpagtm <pushpagautam529@gmail.com>
2023-12-08 13:48:10 +0545

    created register page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+function RegisterUser(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function registerUser(event) {
+    event.preventDefault();
+    await axios
+      .post("/api/register", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Name"
+        />
+        <br />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </>
+  );
+}
+
+export default RegisterUser;
