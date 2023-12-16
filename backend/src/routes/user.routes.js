fbe9cad335ae40a9fb0c90d20c0511c9d86921fb
Pushpagtm <pushpagautam529@gmail.com>
2023-12-16 10:17:18 +0545

    chnages in controller

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 // import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
@@ -16,5 +20,6 @@ router.route("/register").post(
   // ]),
   registerUser
 );
-
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
