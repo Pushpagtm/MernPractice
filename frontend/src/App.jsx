6e764cb2296d80a9e1abe4e57fd4bed674a7a3fe
Pushpagtm <pushpagautam529@gmail.com>
2024-02-22 13:07:56 +0545

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RegisterUser from "./pages/Register";
 function App() {
-  // const [jokes, setJokes] = useState([]);
-
-  // useEffect(() => {
-  //   axios
-  //     .get("/api/jokes")
-  //     .then((res) => {
-  //       setJokes(res.data.jokes); // Access the "jokes" array from res.data
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }); // Add an empty dependency array to ensure useEffect runs only once
-
   return (
     <div>
-      {/* {jokes.map((data) => (
-        <div key={data.id}>
-          <h1>{data.title}</h1>
-          <h2>{data.content}</h2>
-        </div>
-      ))} */}
       <Router>
         <Routes>
           <Route path="/register" exact component={RegisterUser} />
